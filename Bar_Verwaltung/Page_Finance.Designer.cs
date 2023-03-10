f6c15bd24ad72015febc33a937fe30d0db2a2b27
Markus Hauser <markus.hauser@hak-kitz.ac.at>
2023-03-10 09:39:58 +0100

    Page_Finance

--- Bar_Verwaltung/Page_Finance.Designer.cs
+++ Bar_Verwaltung/Page_Finance.Designer.cs
@@ -28,20 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.lb_date = new System.Windows.Forms.Label();
             this.lb_titel = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // flowLayoutPanel1
-            // 
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(14, 17);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(635, 317);
-            this.flowLayoutPanel1.TabIndex = 0;
             // 
             // lb_date
             // 
@@ -55,7 +49,7 @@
             // lb_titel
             // 
             this.lb_titel.AutoSize = true;
-            this.lb_titel.Location = new System.Drawing.Point(666, 79);
+            this.lb_titel.Location = new System.Drawing.Point(666, 73);
             this.lb_titel.Name = "lb_titel";
             this.lb_titel.Size = new System.Drawing.Size(72, 15);
             this.lb_titel.TabIndex = 2;
@@ -82,16 +76,42 @@
             this.textBox3.Size = new System.Drawing.Size(113, 23);
             this.textBox3.TabIndex = 5;
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(14, 23);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(635, 311);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(676, 277);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 70);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Page_Finance
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.lb_titel);
             this.Controls.Add(this.lb_date);
-            this.Controls.Add(this.flowLayoutPanel1);
             this.Name = "Page_Finance";
             this.Size = new System.Drawing.Size(800, 375);
             this.ResumeLayout(false);
@@ -100,12 +120,12 @@
         }
 
         #endregion
-
-        private FlowLayoutPanel flowLayoutPanel1;
         private Label lb_date;
         private Label lb_titel;
         private TextBox textBox1;
         private TextBox textBox2;
         private TextBox textBox3;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Button button1;
     }
 }
